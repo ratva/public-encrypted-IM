0a31f796209b515fd209c590adc123075ffe48e2
Avtar <ratva@users.noreply.github.com>
2024-03-24 18:08:09 -0400

    RSA key pair generation implemented

--- signer.py
+++ signer.py
@@ -1,47 +1,29 @@
+import argparse
+import select
+import socket
+import sys
+# from math import ceil as ceil
+# from Crypto.Cipher import AES as AES
+# from Crypto.Hash import HMAC as HMAC
+from Crypto.Hash import SHA256 as SHA256
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 from Crypto.PublicKey import RSA
 
-mykey = RSA.generate(4096)
-with open("mypublickey.pem", "wb") as f:
-    data = mykey.public_key().export_key()
 
-def mypad(somenum):
-    return '0' * (4-len(str(somenum))) + str(somenum)
 
-pwd = b'secret'
-with open("myprivatekey.pem", "wb") as f:
-    data = mykey.export_key(passphrase=pwd,
-                                pkcs=8,
-                                protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
-                                prot_params={'iteration_count':131072})
-    f.write(data)
-# and reimport it later:
-
-pwd = b'secret'
-with open("myprivatekey.pem", "rb") as f:
-    data = f.read()
-    mykey = RSA.import_key(data, pwd)
-
-# For the second programming assignment, you will write a program that (1) generates an RSA
-# keypair, (2) writes the public portion of the key to a file, and (3) sends a message over a network,
-# followed by its signature.
+# For the second programming assignment, you will write a program that (1) generates an RSA keypair, (2) writes the public portion of the key to a file, and (3) sends a message over a network, followed by its signature.
 # • Your program must be called signer.py (in lowercase).
 # • Your program should have the following command-line options:
 # signer.py --genkey | --c hostname --m message
-# That is, the program works in two modes. If the --genkey option is specified, your program
-# should generate a new RSA keypair. The public key must be stored in a file in the current
-# working directory called mypubkey.pem (Example here). That is, you should save the key
-# as the contents of this file. Note that mypubkey.pem must not contain the private key. (You
-# may want to store the private key in another file because you’ll need it to sign messages.)
-# If the -c option is specified, then signer.py should open a TCP connection to port 9998
-# (not 9999) of hostname and send via that connection a signed copy of message.
+# That is, the program works in two modes. If the --genkey option is specified, your program should generate a new RSA keypair. The public key must be stored in a file in the current working directory called mypubkey.pem (Example here). That is, you should save the key as the contents of this file. Note that mypubkey.pem must not contain the private key. (You may want to store the private key in another file because you’ll need it to sign messages.)
+# If the -c option is specified, then signer.py should open a TCP connection to port 9998 (not 9999) of hostname and send via that connection a signed copy of message.
 # • You should generate 4096 bit keys.
-# • When --genkey is issued, you should store the public key in PEM format. If key is the
-# keypair generated using the Crypto.PublicKey.RSA module of PyCryptodome, then you can
-# get the public key portion of that key via:
+# • When --genkey is issued, you should store the public key in PEM format. If key is the keypair generated using the Crypto.PublicKey.RSA module of PyCryptodome, then you can get the public key portion of that key via:
 # pubkey_pem = key.publickey().export_key()
-# • Your signatures must be in PKCS#1 v1.5 format. This is done for you, so long as you use the
-# appropriate PyCrypto Signature module (you’ll really want to visit that URL), you should
-# be fine.
+# • Your signatures must be in PKCS#1 v1.5 format. This is done for you, so long as you use the appropriate PyCrypto Signature module (you’ll really want to visit that URL), you should be fine.
 # • If you are implementing RSA yourself, you’re doing it wrong.
 # • You MUST send signed messages in EXACTLY the following format:
 # – the length of the message, in bytes, as a padded 4-byte string. You can generate this via:
@@ -75,17 +57,204 @@ with open("myprivatekey.pem", "rb") as f:
 # 9b51f0d5e867eaed79dbfd07e22015c1910ef97122a4aeb70f47cbf175fb9
 # fc365075b9afa455182e9c5030cfeb089edcf16e47a5637b35e586bbf3373
 # 33978d
-# (above, the newlines are added for readability; your code should not send newlines unless
-# they are part of the message.)
-# Note the format of the above: the length of the string (“0011”; i.e., 11), padded to 4 bytes;
-# the string itself; the length of the signature (“1024”; i.e., 1024), padded to 4 bytes; and the
-# signature of the hash of the string, in hex. Note also that there are no delimiters. This is why
-# we have the padded lengths in there.
+# (above, the newlines are added for readability; your code should not send newlines unless they are part of the message.)
+# Note the format of the above: the length of the string (“0011”; i.e., 11), padded to 4 bytes; the string itself; the length of the signature (“1024”; i.e., 1024), padded to 4 bytes; and the signature of the hash of the string, in hex. Note also that there are no delimiters. This is why we have the padded lengths in there.
 # Hints
-# • If you are confused about any of the above, don’t guess. Post a request for clarification to
-# Piazza.
-# • You are not required to build your own server for this problem. The autograder will provide
-# the server that you submit the signature to for evaluation. For testing purposes, you can use
-# nc -l 127.0.0.1 9999 to set up a listening socket on port 9998 that will print to any data sent to
-# that socket to the command line. This will allow you to see whether everything is being sent
-# as expected.
+# • If you are confused about any of the above, don’t guess. Post a request for clarification to Piazza.
+# • You are not required to build your own server for this problem. The autograder will provide the server that you submit the signature to for evaluation. For testing purposes, you can use nc -l 127.0.0.1 9999 to set up a listening socket on port 9998 that will print to any data sent to that socket to the command line. This will allow you to see whether everything is being sent as expected.
+
+# HOST = 127.0.0.1
+PORT = 9998
+
+def genkey():
+    mykey = RSA.generate(4096, randfunc=get_random_bytes, e=65537)
+    print(mykey)
+    print(mykey.public_key())
+
+    # Private RSA key
+    with open("myprivatekey.pem", "wb") as f:
+        data = mykey.export_key(format='PEM', passphrase=None, pkcs=1.5, protection=None, randfunc=None, prot_params=None)
+        f.write(data)
+        
+    with open("mypublickey.pem", "wb") as f:
+        data = mykey.public_key().export_key(format='PEM', passphrase=None, pkcs=1.5, protection=None, randfunc=None, prot_params=None)
+        f.write(data)
+
+    # def mypad(somenum):
+    #     return '0' * (4-len(str(somenum))) + str(somenum)
+
+    # print(int(mypad(5)))
+
+    # pwd = b'secret'
+    # with open("myprivatekey.pem", "wb") as f:
+    #     data = mykey.export_key(passphrase=pwd,
+    #                                 pkcs=8,
+    #                                 protection='PBKDF2WithHMAC-SHA512AndAES256-CBC',
+    #                                 prot_params={'iteration_count':131072})
+    #     f.write(data)
+    # # and reimport it later:
+
+    # pwd = b'secret'
+    # with open("myprivatekey.pem", "rb") as f:
+    #     data = f.read()
+    #     mykey = RSA.import_key(data, pwd)
+pass
+
+def start_client(hostname):
+    # Create an IPv4 TCP socket for the client
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Set the socket address to be reusable
+    client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Connect socket to an existing hostname on port 9999, else fail and stop program.
+    try:
+        client_socket.connect((hostname, PORT))
+        # Comment out print statements for gradescope
+        # print(f'Connected to server: {hostname}')
+    except ValueError:
+        # Comment out print statements for gradescope
+        # print(f"Failed to connect: {err}")
+        client_socket.close()
+        sys.exit(0)
+    return client_socket
+
+def encode_message(message, K1,K2):
+    # iv + E_k1(len(m)) + HMAC_k2(iv + E_k1(len(m))) + E_k1(m) + HMAC_k2(E_k1(m))
+
+    IV = randb(16)
+
+    # Create a 256-bit key for AES encryption/decryption by using the SHA256 hash of the K1 key.
+    K1_256 = SHA256.new(K1.encode()).digest()
+    # The encode() method turns the string into bytes, needed for the SHA256.new() method.
+    # The digest() method returns the final 256 bit hash in binary.
+
+    # Create an HMAC instance by using the SHA256 hash of the K2 key.
+    K2_HMAC = HMAC.new(K2.encode(), digestmod=SHA256)
+    # Repeat for the message
+    K2_HMAC_message = HMAC.new(K2.encode(), digestmod=SHA256)
+
+    # Create an AES instance for encryption/decryption by using the SHA256 hash of the K1 key.
+    K1_cipher = AES.new(key=K1_256, mode=AES.MODE_CBC, iv=IV)
+
+    # Create a binary object which is the encryption of the length of the message (padded)
+    K1_length = K1_cipher.encrypt(pad(len(message).to_bytes(15,'little'), AES.block_size))
+    # Create a binary object which is the encryption of the message (padded)
+    K1_message = K1_cipher.encrypt(pad(message.encode('utf-8'), AES.block_size))
+
+    HMAC_IV_length = K2_HMAC.update(IV + K1_length).digest()
+    HMAC_message = K2_HMAC_message.update(K1_message).digest()
+    enc_message = IV + K1_length + HMAC_IV_length + K1_message + HMAC_message
+    return enc_message
+
+def decode_message(enc_message, K1, K2, sock):
+    # Extract the IV
+    IV = enc_message[:16]
+
+    # Extract K1_length
+    K1_length = enc_message[16:32]  # K1_length is 16 bytes (128 bits)
+
+    # Extract the HMAC of IV + K1_length
+    K2_HMAC_IV_K1_length = enc_message[32:64]  # The HMAC used SHA256 so it is 32 bytes (256 bits)
+
+    # Create a 256-bit key for HMAC computation by using the SHA256 hash of the K2 key.
+    K2_HMAC = HMAC.new(K2.encode(), digestmod=SHA256)
+    HMAC_IV_length = K2_HMAC.update(IV + K1_length).digest()
+
+    # Validate that IV and length are unchanged
+    if K2_HMAC.digest() != K2_HMAC_IV_K1_length:
+        print("ERROR: HMAC verification failed")
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        sys.exit(0)
+
+    K1_256 = SHA256.new(K1.encode()).digest()
+    K1_cipher = AES.new(key=K1_256, mode=AES.MODE_CBC, iv=IV)
+
+    try:
+        length = int.from_bytes(unpad(K1_cipher.decrypt(K1_length), AES.block_size),'little')
+    except Exception:
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        sys.exit(0)
+
+    enc_length = ceil(length/AES.block_size)*AES.block_size
+
+    K1_message = enc_message[64:64+enc_length]
+    HMAC_message = enc_message[64+enc_length:96+enc_length]
+
+    K2_HMAC_message_inst = HMAC.new(K2.encode(), digestmod=SHA256)
+    K2_HMAC_message = K2_HMAC_message_inst.update(K1_message).digest()
+
+    if K2_HMAC_message != HMAC_message:
+        print("ERROR: HMAC verification failed")
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        sys.exit(0)
+
+    message = unpad(K1_cipher.decrypt(K1_message), AES.block_size).decode('utf-8')
+
+    return message
+
+def main():
+    # Create argument parser to handle command line options
+    parser = argparse.ArgumentParser(description='Generate key or connect to server.')
+    # Set it so that only one of server or client options can be selected
+    group = parser.add_mutually_exclusive_group(required=True)
+    # If server, set arg.s to True
+    group.add_argument('--genkey', action='store_true', help='Generate a RSA key pair')
+    # If client, set arg.c to the provided hostname argument
+    group.add_argument('--c', metavar='hostname', type=str, help='Start as client and connect to hostname')
+    # Add arguments for the confidentiality and authenticity keys
+    parser.add_argument('--m', metavar='message', type=str, help='Message',required=False)
+    # Parse inputs from the command line when the script was called
+    args = parser.parse_args()
+
+    if args.genkey:
+        # Start the server
+        genkey()
+    else:
+        # Start the client and connect to the hostname provided as an argument
+        sock = start_client(args.c)
+    
+        try:
+            # Loop communication until the connection is closed
+            while True:
+                # Wait until either the stdin or the socket have a message - both are in the incoming data list and so select waits until either is populated.
+                readable, _, _ = select.select([sys.stdin, sock], [], [])
+                # Iterate over each readable source to manage if both stdin and the socket have new messages at the same time.
+                for source in readable:
+                    if source == sock:
+                        # Read message from socket
+                        enc_message = sock.recv(4096)
+                        # If message is empty it means the connection is closed, so close socket and exit the console
+                        # print(enc_message)
+                        if not enc_message:
+                            print('Connection closed')
+                            sock.shutdown(socket.SHUT_RDWR)
+                            sock.close()
+                            sys.exit(0)
+
+                        message = decode_message(enc_message, sock)
+
+
+                        # Otherwise decode the message from bytes to a string and print to console WITHOUT adding a new line \n character
+                        print(message, end='')
+                        # Flush the output to prevent issues with gradescope
+                        sys.stdout.flush()
+
+                    else:
+                        # Read message from stdin
+                        message = sys.stdin.readline()
+                        # Encrypt message
+                        enc_message = encode_message(message = message, K1=K1, K2=K2)
+                        # Encode the message to bytes and send it to the socket
+                        sock.sendall(enc_message)
+
+        except KeyboardInterrupt:
+            # Console will show ^C
+            print(' received - closing connection')
+            sock.shutdown(socket.SHUT_RDWR)
+            sock.close()
+            sys.exit(0)
+        
+if __name__ == "__main__":
+    main()
